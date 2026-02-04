524b87b9e405caea5a463728debd1da26147e59d
Jhefferson <jhefferson.santos@marata.com.br>
2026-02-04 11:56:24 -0300

    hotfix: add api key

--- src/services/category/categoryService.js
+++ src/services/category/categoryService.js
@@ -2,11 +2,12 @@ import api from '../api';
 import { CATEGORY_PATHS, buildGetAllCategoriesParams } from './routes';
 
 const CATEGORY_API_BASE = process.env.REACT_APP_API_URL + '/category';
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 export const categoryService = {
   getAllCategoriesByFilter: (
     filter = { name: '', category_id: '',  owner_id: '', page: 1, page_size: 10 },
-    { token = null, apiKey = 'shop_api_123' } = {}
+    { token = null, apiKey = API_KEY } = {}
   ) => {
     const params = buildGetAllCategoriesParams(filter);
     const headers = { 'X-API-KEY': apiKey };
--- src/services/company/companyService.js
+++ src/services/company/companyService.js
@@ -2,11 +2,12 @@ import api from '../api';
 import { COMPANY_PATHS, buildGetAllCompaniesParams } from './routes';
 
 const COMPANY_API_BASE = process.env.REACT_APP_API_URL + '/company';
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 export const companyService = {
   getAllCompaniesByFilter: (
     filter = { id: '', name: '', page: 1, page_size: 10 },
-    { token = null, apiKey = 'shop_api_123' } = {}
+    { token = null, apiKey = API_KEY } = {}
   ) => {
     const params = buildGetAllCompaniesParams(filter);
     const headers = { 'X-API-KEY': apiKey };
